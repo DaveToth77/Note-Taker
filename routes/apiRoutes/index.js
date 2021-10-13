fdf73eff64abcc70a5cd40d9d0068ce478f1e270
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-13 11:52:58 -0400

    gitignore is broken

--- routes/apiRoutes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const notes = require('../apiRoutes/noteRoutes');
-
-router.use(notes);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,79 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+// parse incoming string or array data
+app.use(express.urlencoded({
+    extended: true
+}));
+// parse incoming JSON data
 app.use(express.json());
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.static('public'));
+
+const {
+    notes
+} = require('./db/db.json');
+
+app.get('/api/notes', (req, res) => {
+    // console.log(notes)
+    res.json(notes);
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+// app.delete('/api/notes/:id', (req, res) => {
+    
+// })
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({
+            notes: notesArray
+        }, null, 2)
+    );
+
+    return note;
+}
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
 
 
 
